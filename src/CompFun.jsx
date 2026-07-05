bd1d9b4f569e3567976346f2de3cffc559ccce98
stomeetings <stomeetings@gmail.com>
2026-07-05 09:11:47 +0500

    Initial commit

---
+++ src/CompFun.jsx
@@ -0,0 +1,15 @@
+export function CompFun() {
+    return (
+        <>
+        <h1>Functional components tutorials</h1>;     
+        
+        </>    
+    )
+
+}
+export function CompFun2() {
+    return <h3>Sub Heading-1</h3>;
+}
+
+export const flower="Rose";
+//export default CompFun;
---
+++ src/NeCompo.jsx
@@ -0,0 +1,5 @@
+export function NeCompo(){
+    return <h1>New Component</h1>;
+}
+
+export default NeCompo;
